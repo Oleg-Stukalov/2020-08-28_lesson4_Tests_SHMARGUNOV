a25e6a10067e0ae94f2ec084de2bf42c36e7a107
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-30 17:10:30 +0300

    minor changes

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import requests
 
 def multiplication_int(a, b):
     return a * b
+
 def multiplication_string(line, n):
     return line * n
+
 def get_name_hero_by_id(_id):
     r = requests.get(f'https://superheroapi.com/api/2619421814940190/{_id}')
     result = r.json()
     if 'name' in result:
         name = result['name']
         return name
+
 print(get_name_hero_by_id(70))
 # name = input('Введите имя ')
 #
--- module1.py
+++ module1.py
@@ -5,15 +5,20 @@ from main import multiplication_int, multiplication_string, get_name_hero_by_id
 class TestSomething:
     def setup(self):
         print("method setup")
+
     def teardown(self):
         print("method teardown")
+
     def test_numbers_3_4(self):
         assert multiplication_int(3, 4) == 12
+
     @pytest.mark.parametrize("first, second, result", [(3, 4, 12), (5, 0, 0), (-5, 10, -50)])
     def test_numbers(self, first, second, result):
         assert multiplication_int(first, second) == result
+
     def test_strings_a_3(self):
         assert multiplication_string('a', 3) == 'aaa'
+
     @pytest.mark.parametrize("id_, expected_result", [(70, 'Batman'), (644, 'Superman'), ('asd', None), ('', None)])
     def test_check_name_hero(self, id_, expected_result):
         assert get_name_hero_by_id(id_) == expected_result
